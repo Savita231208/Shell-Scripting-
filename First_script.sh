d83a51f75fc82de00a0d558e99f404d35f76f1fb
Savita Kumari <gsavita203@gmail.com>
2026-03-25 15:13:47 +0530

    Added README file

---
+++ First_script.sh
@@ -0,0 +1,12 @@
+# Shell Scripting Project
+#!/bin/bash
+#This is my first Shell script
+
+echo "Hello, World!"
+echo "Welcome to shell scripting"
+
+#Taking input from user
+echo "Enter your name:"
+read name
+echo "Hello $name, nice to meet you!"
+
